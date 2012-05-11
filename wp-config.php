abeb350666cc114bd848400a20da98349e6da2c5
Ana Aires <ana.aires@gmail.com>
2012-05-11 17:49:04 +0200

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -19,10 +19,10 @@
 define('DB_NAME', 'solar');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'username');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'xpto123');
+define('DB_PASSWORD', 'password');
 
 /** MySQL hostname */
 define('DB_HOST', '127.0.0.1');
