288eabcd9565693240c7b86d9f80402336b8a7c1
unknown <Dylan@hp-PC.(none)>
2012-05-22 12:21:30 +0100

    footer da galeria

---
+++ wp-content/themes/mosaico/includes/sliders/mosaico8bottomgallery.php
@@ -0,0 +1,56 @@
+<div id="mosaic" style="height:129px; overflow:hidden">
+
+
+    <div class="mosaicitembottom41" id="mosaicgallery1">
+        <div class="galleryimg1"><img src="<?php bloginfo('template_url'); ?>/images/gallerythumb1.jpg"/></div>
+        <div class="galleryimg2"><img src="<?php bloginfo('template_url'); ?>/images/gallerythumb2.jpg"/></div>
+                    
+    </div>
+
+<!-- end mosaic #1 -->
+
+
+
+    
+    <div class="mosaicitembottom42" id="mosaicgallery2">
+        <div class="galleryimg1"><img src="<?php bloginfo('template_url'); ?>/images/gallerythumb1.jpg"/></div>
+        <div class="galleryimg2"><img src="<?php bloginfo('template_url'); ?>/images/gallerythumb2.jpg"/></div>
+                     
+    </div>
+
+<!-- end mosaic #2 -->
+
+
+
+
+    
+    <div class="mosaicitembottom43" id="mosaicgallery3">
+        <div class="galleryimg1"><img src="<?php bloginfo('template_url'); ?>/images/gallerythumb1.jpg"/></div>
+        <div class="galleryimg2"><img src="<?php bloginfo('template_url'); ?>/images/gallerythumb2.jpg"/></div>
+                    
+    </div>
+
+<!-- end mosaic #3 -->
+
+
+
+<div class="mosaicitem44" id="reserva">
+        <div class="inpost2">
+            <form action="">
+            <div class="reserva"><?php _e('Book now','solar'); ?>
+                <input class="search" type="submit" value="<?php _e('Search','solar'); ?>">
+            </div>
+            </form>
+        </div>
+    </div>
+
+
+<!-- end mosaic #4 -->
+
+
+
+
+
+
+</div>
+<div style="clear: both;"></div>
