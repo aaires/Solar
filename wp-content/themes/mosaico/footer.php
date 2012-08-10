1b38d4d4429c72387c103836ad6d4a9f1c222ae2
aaires <ana.aires@gmail.com>
2012-08-10 15:14:29 +0100

    small change

--- wp-content/themes/mosaico/footer.php
+++ wp-content/themes/mosaico/footer.php
@@ -1,20 +1,3 @@
-<!--   <div id="homebottomrow" class="row">
-        
-       <div class="twelvecol">
-       
-                    
-           <?php /*if(is_home()) 
-           		{
-    			
-					echo get_template_part('/includes/sliders/mosaico8bottom' );;	
-											
-				} */
-			?>
-                   
-       	</div>
-    </div>
-   -->  
-    
 <div class="row container">
 	<?php get_template_part('/includes/sliders/mosaico8bottom' ); ?> 
 </div>
