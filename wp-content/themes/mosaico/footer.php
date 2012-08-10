a82dc36359f2269a876dbc86a4fcf369d9ad3435
aaires <ana.aires@gmail.com>
2012-08-10 12:40:07 +0100

    cleaning footer

--- wp-content/themes/mosaico/footer.php
+++ wp-content/themes/mosaico/footer.php
@@ -1,41 +1,3 @@
- 
- 
- <!--     <div class="container body3 raster-bottom"> 
-    
-        <div class="row">	
-             <div class="eightcol first">
-    
-                    <h1 class="tagline"><?php echo get_option('themnific_tagline');?></h1>
-
-        	</div>
-            
-            <div class="fourcol"> 
-            
-    			<?php //get_template_part('/includes/uni-social');?>
-                
-    		</div>
-    
-    	</div>
-        
-    </div>  
- -->
- <!-- 
-
-	<div id="archives" class="body2" style="<?php if(!is_404() && (!is_search() || have_posts())) echo 'display:none'; ?>" >
-
-        <div class="container"  style="padding-bottom:0px"> 
-            <div class="row">
-            
-                <?php //get_template_part('/includes/uni-more');?>
-    
-            </div> 
-        </div>
-    
-    </div><!-- end #archives --> 
- <!--    <div id="toggleArchives" class="rad boxshadow" onclick="toggleArchives();">&darr; More &darr;</div> 
-    
-   -->    
-     
   <div id="homebottomrow" class="row">
         
        <div class="twelvecol">
@@ -52,15 +14,6 @@
        	</div>
     </div>
     
-        
-<!--
-        <?php if (get_option('themnific_dis_foowidgets') <> "true") { ?>
-            <?php get_template_part('/includes/uni-bottombox');?>
-        <?php } ?>
-        	
-
-
--->
 <div id="footer">
         
 	<div id="lastfooter" class="row">
