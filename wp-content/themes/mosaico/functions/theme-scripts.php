ebaee5ae6e35aa28ace888b21faf8474d5f718cd
aaires <ana.aires@gmail.com>
2012-08-10 11:57:18 +0100

    ie not loading script

--- wp-content/themes/mosaico/functions/theme-scripts.php
+++ wp-content/themes/mosaico/functions/theme-scripts.php
@@ -10,14 +10,14 @@ if ( ! function_exists( 'themnific_add_javascript' ) ) {
 		wp_enqueue_script( 'jquery.hoverIntent.minified', get_template_directory_uri().'/js/jquery.hoverIntent.minified.js');
 		wp_enqueue_script( 'css3-mediaqueries', get_template_directory_uri().'/js/css3-mediaqueries.js');
 		wp_enqueue_script('tabs', get_stylesheet_directory_uri() .'/js/tabs.js');
-		wp_enqueue_script('ownScript', get_stylesheet_directory_uri() .'/js/ownScript.js','','', false); /*aaires was true*/
+		wp_enqueue_script('ownScript', get_stylesheet_directory_uri() .'/js/ownScript.js','','', true);
 
 
 		// filterable scripts
 		if (is_page_template('template-filterable3.php') || is_page_template('template-filterable4.php') || is_home() || is_archive()){
 		wp_enqueue_script('mootools-core-1.4.5', get_stylesheet_directory_uri() .'/js/mediabox/mootools-core-1.4.5.js');
 		wp_enqueue_script('mediabox.advanced', get_stylesheet_directory_uri() .'/js/mediabox/mediabox-1.2.4.js');
-		wp_enqueue_script('filterable.pack', get_template_directory_uri().'/js/filterable.pack.js','','', false); /* aaires was true*/ 
+		wp_enqueue_script('filterable.pack', get_template_directory_uri().'/js/filterable.pack.js','','', true); 
 		}
 		
 		// Load Mediabox scripts
